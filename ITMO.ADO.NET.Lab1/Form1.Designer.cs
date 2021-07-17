4df392e3aaa8270037df7382b8e2083a7dde16d8
Alexander (BuHu-IIyx) <Buhu.iiyxs@gmail.com>
2021-07-17 03:36:19 +0300

    Add project files.

---
+++ ITMO.ADO.NET.Lab1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace ITMO.ADO.NET.Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectionListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectDBToolStripMenuItem,
+            this.disconnectDBToolStripMenuItem,
+            this.connectionListToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectDBToolStripMenuItem
+            // 
+            this.connectDBToolStripMenuItem.Name = "connectDBToolStripMenuItem";
+            this.connectDBToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.connectDBToolStripMenuItem.Text = "Connect DB";
+            this.connectDBToolStripMenuItem.Click += new System.EventHandler(this.connectDBToolStripMenuItem_Click);
+            // 
+            // disconnectDBToolStripMenuItem
+            // 
+            this.disconnectDBToolStripMenuItem.Name = "disconnectDBToolStripMenuItem";
+            this.disconnectDBToolStripMenuItem.Size = new System.Drawing.Size(120, 24);
+            this.disconnectDBToolStripMenuItem.Text = "Disconnect DB";
+            this.disconnectDBToolStripMenuItem.Click += new System.EventHandler(this.disconnectDBToolStripMenuItem_Click);
+            // 
+            // connectionListToolStripMenuItem
+            // 
+            this.connectionListToolStripMenuItem.Name = "connectionListToolStripMenuItem";
+            this.connectionListToolStripMenuItem.Size = new System.Drawing.Size(121, 24);
+            this.connectionListToolStripMenuItem.Text = "Connection list";
+            this.connectionListToolStripMenuItem.Click += new System.EventHandler(this.connectionListToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectionListToolStripMenuItem;
+    }
+}
+
